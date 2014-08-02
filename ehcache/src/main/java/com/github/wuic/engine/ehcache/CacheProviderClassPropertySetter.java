c4a74bb212b6e524a98a66c142fe635f1140a2fd
gdrouet <guidrouet@gmail.com>
2014-08-02 14:03:06 +0200

    New refactored config API based on annotation processors for https://github.com/wuic/wuic/issues/115

--- ehcache/src/main/java/com/github/wuic/engine/ehcache/CacheProviderClassPropertySetter.java
+++ ehcache/src/main/java/com/github/wuic/engine/ehcache/CacheProviderClassPropertySetter.java
@@ -40,7 +40,7 @@ package com.github.wuic.engine.ehcache;
 
 import com.github.wuic.ApplicationConfig;
 import com.github.wuic.engine.AbstractEngineBuilder;
-import com.github.wuic.util.PropertySetter;
+import com.github.wuic.config.PropertySetter;
 
 /**
  * <p>
